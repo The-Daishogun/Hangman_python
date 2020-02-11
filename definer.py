3387648aa7a7ce7678919f0fc24d936e4814d1d1
Mohammadreza Varasteh <58051486+The-Daishogun@users.noreply.github.com>
2020-02-11 20:22:27 +0330

    Add files via upload

--- definer.py
+++ definer.py
@@ -1,32 +1,33 @@
-import urllib3
-import re
-
+import requests
 from bs4 import BeautifulSoup
+from random import randint
 
 
-def cleanhtml(raw_html):
-    cleanr = re.compile('<.*?>')
-    cleantext = re.sub(cleanr, '', raw_html)
-    return cleantext
+def write_word_file():
+    word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+    response = requests.get(word_site)
+    WORDS = response.content.splitlines()
+    f = open('words.txt', "w")
+    for item in WORDS:
+        item = str(item)[1:].replace("'", "")
+        f.write(item + "\n")
+    f.close()
 
 
-def find(word):
-    url = "https://www.thefreedictionary.com/" + word
-    req = urllib3.PoolManager()
-    result = req.request("GET", url)
-    soup = BeautifulSoup(result.data, 'html.parser')
-    content = soup.find_all(class_='ds-list')
-    content = cleanhtml(str(content[0])).replace(":", "")
-    content = content.lstrip()
+def get_word():
+    f = open('words.txt', "r")
+    WORDS = f.readlines()
+    word = WORDS[randint(0, len(WORDS))]
+    word = word.replace("\n", "")
 
-    if content[0].isnumeric():
-        content = content[2:]
-
-    return content.lstrip()
+    return word
 
 
-def debug():
-    a = input()
-    print(find(a))
-
-
+def definer(word):
+    defs = []
+    page = requests.get("https://www.ldoceonline.com/dictionary/" + word)
+    soap = BeautifulSoup(page.content, "html.parser")
+    text = soap.find_all(class_="DEF")[:3]
+    for count, item in enumerate(text):
+        defs.append(str(count + 1) + ". " + item.get_text().strip())
+    return defs
--- hangman.py
+++ hangman.py
@@ -1,64 +1,93 @@
 import os
-from random_word import RandomWords
-from definer import find
+import definer
 
 
 def get_word():
-    r = RandomWords()
-    word = r.get_random_word(hasDictionaryDef="true", excludePartOfSpeech="verb,adverb,pronoun,preposition,adjective")
+    word = definer.get_word()
     return word.title()
 
 
-print("loading Game...")
+def end_game(end=True):
 
-while True:
-    try:
-        word = get_word()
-        definition = find(word).title()
-    except IndexError:
-        continue
-    break
-
-player_word = ["*" for i in word]
-miss_list = []
-
-
-def print_player_word():
-    print("Word: " + "".join(player_word))
-
-
-def print_miss_list():
-    print("Misses: " + "".join(miss_list))
-
-
-os.system('cls' if os.name == 'nt' else 'clear')
-
-while "*" in player_word:
-    if len(miss_list) == len(word):
-        print("You Lose!\nThe Word was: {}".format(word))
-        print("Definition: {}".format(definition))
-        print("Definition by The Free Dictionary.com")
-        break
+    if end:
+        print("Congrats! You Win!")
     else:
-        print_player_word()
-        print_miss_list()
-        print()
+        print("You Lose!")
 
-        char = input("Input your guess:\n")
-        if char.isnumeric() or char == "" or len(char) > 1:
-            while not char.isalpha() or char == "" or len(char) > 1:
-                print("Please Enter a Letter!")
-                char = input("Input your guess:\n")
-
-        if char.lower() in word.lower():
-            for i in range(len(word)):
-                if word[i].lower() == char.lower():
-                    player_word[i] = word[i]
-        else:
-            miss_list.append(char)
-    os.system('cls' if os.name == 'nt' else 'clear')
-else:
-    print("Congrats! You Win!")
     print("The Word was: {}".format(word))
-    print("Definition: {}".format(definition))
-    print("Definition by The Free Dictionary.com")
+    print("Definition:")
+    for item in definition:
+        print(item.title())
+    print("\nDefinition by ldoceonline.com".center(10))
+
+
+try:
+    f = open("words.txt")
+except IOError:
+    definer.write_word_file()
+finally:
+    f.close()
+
+game_state = True
+
+while game_state:
+    print("loading Game...")
+
+    while True:
+
+        word = get_word()
+        definition = definer.definer(word)
+        if not definition:
+            continue
+        else:
+            break
+
+    player_word = ["*" for i in word]
+    miss_list = []
+
+
+    def print_player_word():
+        print("Word:\t" + "".join(player_word))
+
+
+    def print_miss_list():
+        print("Misses:\t" + "".join(miss_list))
+
+
+    def print_rem_chances():
+        print("Remaining Chances:", (len(word) + 1) - len(miss_list))
+
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    while "*" in player_word:
+        if len(miss_list) == len(word) + 1:
+            end_game(False)
+            break
+        else:
+            print_player_word()
+            print_miss_list()
+            print_rem_chances()
+            print()
+
+            char = input("Input your guess:\n")
+            if char.isnumeric() or char == "" or len(char) > 1:
+                while not char.isalpha() or char == "" or len(char) > 1 or char in miss_list:
+                    print("Please Enter a Letter!")
+                    char = input("Input your guess:\n")
+
+            if char.lower() in word.lower():
+                for i in range(len(word)):
+                    if word[i].lower() == char.lower():
+                        player_word[i] = word[i]
+            elif char.lower() in miss_list:
+                pass
+            else:
+                miss_list.append(char)
+        os.system('cls' if os.name == 'nt' else 'clear')
+    else:
+        end_game()
+
+    replay = input("\nreplay? (y/n)\n")
+    if replay.lower() == 'n':
+        game_state = False
